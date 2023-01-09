78d9b9932ef4790f44d6da703e25be2b3a162815
Jun Ohashi <71494598+j-ohashi@users.noreply.github.com>
2023-01-09 20:48:54 +0900

    存在しないコンテナへのリンクを削除、初回起動時にcomposerをインストールするよう追加

--- docker/php-fpm/docker-app-entrypoint.sh
+++ docker/php-fpm/docker-app-entrypoint.sh
@@ -9,6 +9,11 @@ fi
 if [ ! -e '/tmp/checked' ]; then
     touch /tmp/checked
     echo "first boot"
+    cd /tmp
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    mv composer.phar /usr/local/bin/composer
 fi
 
 docker-app-healthcheck.sh
